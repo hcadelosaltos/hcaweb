2a2023ab8573ab9cbdfcf46eb89e16e384c87847
Emmanuel Valenzuela <manuvaglz@gmail.com>
2025-09-19 17:42:12 -0600

    HomeServices

---
+++ src/data/homeServices.ts
@@ -0,0 +1,29 @@
+export const homeServices = [
+    {
+        title: "Padres provisores",
+        description: "Acompañamos el crecimiento de tu familia desde la prevención",
+        alt: "Services 1",
+        image: "/src/assets/home-services1.png",
+        label: "Servicios",
+        href: "/",
+
+    },
+    {
+        title: "Pacientes conscientes",
+        description: "Porque prevenir, también es cuidarte.",
+        alt: "Services 2",
+        image: "/src/assets/home-services2.png",
+        label: "Servicios",
+        href: "/",
+
+    },
+    {
+        title: "Médicos",
+        description: "Un espacio profesinal para brindar atención de calidad.",
+        alt: "Services 3",
+        image: "/src/assets/home-services3.png",
+        label: "Servicios",
+        href: "/",
+
+    }
+]
