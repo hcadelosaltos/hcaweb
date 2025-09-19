cf9396c63fa6e0a81c3e3864bd30d7156918fc55
Emmanuel Valenzuela <manuvaglz@gmail.com>
2025-09-19 12:57:04 -0600

    HomeHero & initial theme config

---
+++ src/data/nav.ts
@@ -0,0 +1,6 @@
+export const mainNav = [
+    { href: "/", label: "Inicio" },
+    { href: "/servicios", label: "Servicios" },
+    { href: "/especialistas", label: "Especialistas" },
+    { href: "/contacto", label: "Contacto" },
+  ];
