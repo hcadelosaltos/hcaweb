795050fe51da927028c7f97055b4e5e0c250a18c
Emmanuel Valenzuela <manuvaglz@gmail.com>
2025-09-19 13:29:16 -0600

    add prettier

--- src/data/nav.ts
+++ src/data/nav.ts
@@ -1,6 +1,6 @@
 export const mainNav = [
-    { href: "/", label: "Inicio" },
-    { href: "/servicios", label: "Servicios" },
-    { href: "/especialistas", label: "Especialistas" },
-    { href: "/contacto", label: "Contacto" },
-  ];
+  { href: '/', label: 'Inicio' },
+  { href: '/servicios', label: 'Servicios' },
+  { href: '/especialistas', label: 'Especialistas' },
+  { href: '/contacto', label: 'Contacto' },
+];
