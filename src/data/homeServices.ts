1a333896084b361af94983e362a1f25914aa6ca3
Emmanuel Valenzuela <manuvaglz@gmail.com>
2025-09-19 18:21:32 -0600

    Fix images issue on vercel

--- src/data/homeServices.ts
+++ src/data/homeServices.ts
@@ -1,9 +1,13 @@
+import services1 from '../assets/home-services1.png'
+import services2 from '../assets/home-services2.png'
+import services3 from '../assets/home-services3.png'
+
 export const homeServices = [
     {
         title: "Padres provisores",
         description: "Acompañamos el crecimiento de tu familia desde la prevención",
         alt: "Services 1",
-        image: "/src/assets/home-services1.png",
+        image: services1,
         label: "Servicios",
         href: "/",
 
@@ -12,7 +16,7 @@ export const homeServices = [
         title: "Pacientes conscientes",
         description: "Porque prevenir, también es cuidarte.",
         alt: "Services 2",
-        image: "/src/assets/home-services2.png",
+        image: services2,
         label: "Servicios",
         href: "/",
 
@@ -21,7 +25,7 @@ export const homeServices = [
         title: "Médicos",
         description: "Un espacio profesinal para brindar atención de calidad.",
         alt: "Services 3",
-        image: "/src/assets/home-services3.png",
+        image: services3,
         label: "Servicios",
         href: "/",
 
